37b3ed2dd0e5debec81b829b2cb0e63d58653b73
Lorenzo Uriel <lorenzouriel394@gmail.com>
2024-09-05 17:06:58 -0300

    Code add to GAC added

---
+++ helps/gacutil.ps1
@@ -0,0 +1,23 @@
+﻿# Caminho para a pasta que contém os assemblies
+$assemblyDir = "C:\Users\loren\source\repos\faturamento_parceiro\ETL\ETL\Documentation\assemblies"
+
+# Caminho para o gacutil.exe (exemplo: caso ele esteja na pasta do Visual Studio)
+$gacutilPath = "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools"
+
+# Adicionar o caminho do gacutil ao PATH
+$env:PATH += ";$gacutilPath"
+
+# Iterar por cada arquivo .dll na pasta
+Get-ChildItem -Path $assemblyDir -Filter *.dll | ForEach-Object {
+    $assemblyPath = $_.FullName
+    Write-Host "Registrando $assemblyPath no GAC..."
+    
+    # Executar gacutil
+    $gacutilResult = & gacutil.exe -i $assemblyPath
+    
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "Sucesso: $assemblyPath registrado no GAC."
+    } else {
+        Write-Host "Erro: Falha ao registrar $assemblyPath no GAC."
+    }
+}
